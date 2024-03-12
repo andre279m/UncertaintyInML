372d2733b1ee5c668e94455827c6e66e2ab933c7
andre279m <mendes.andr843@gmail.com>
2024-03-12 18:07:30 +0000

    Add logging and save metrics to CSV files

--- datasetBuilding.py
+++ datasetBuilding.py
@@ -5,7 +5,6 @@ import random,logging
 from sklearn.model_selection import StratifiedKFold
 from pathlib import Path
 
-
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',level = logging.INFO,datefmt='%Y-%m-%d %H:%M:%S')
 
 # Files
@@ -30,13 +29,9 @@ setsize = int(len(fulldata8))
 del fulldata8
 
 all_negative_pairs_prots = pd.read_csv('DB/negative_pairs_prots.csv', header=0)
-all_negative_pairs_prots = list(tuple(x) for x in all_negative_pairs_prots.to_numpy())
+all_negative_pairs_prots = list((x[0].split('https://string-db.org/network/9606.ENSP')[1],x[1].split('https://string-db.org/network/9606.ENSP')[1],x[2]) for x in all_negative_pairs_prots.to_numpy())
 
 # Pipeline
-embedCSV = pd.read_csv('DB/embeddings.csv',index_col=0)
-embeddings_array = np.array(list(embedCSV.values))
-dict_embeddings = {embedCSV.index[i]: embeddings_array[i] for i in range(len(embeddings_array))}
-vector_size = embedCSV.shape[1]
 random.seed(42)
 
 logging.info("Starting Static dataset building ...")
@@ -56,24 +51,20 @@ for i in range(800, -1, -200):
     # Creating the dataset
     pairs_prots = []
     for d in data.values:
-        pairs_prots.append(('https://string-db.org/network/' + d[0],'https://string-db.org/network/' + d[1], 1))
+        pairs_prots.append((d[0].split('9606.ENSP')[1],d[1].split('9606.ENSP')[1], d[-1]))
 
     negative_pairs_prots = random.sample(all_negative_pairs_prots, len(pairs_prots))
 
     X, y, sample_weight = [], [], []
     for prot1, prot2, c in pairs_prots:
-        emb_prot1 = dict_embeddings[prot1].reshape(1, vector_size)
-        emb_prot2 = dict_embeddings[prot2].reshape(1, vector_size)
-        hada = np.multiply(emb_prot1, emb_prot2)
-        X.append(hada.tolist()[0])
+        line = [prot1,prot2]
+        X.append(line)
         y.append(1)
         sample_weight.append(c)
 
     for prot1, prot2, label in negative_pairs_prots:
-        emb_prot1 = dict_embeddings[prot1].reshape(1, vector_size)
-        emb_prot2 = dict_embeddings[prot2].reshape(1, vector_size)
-        hada = np.multiply(emb_prot1, emb_prot2)
-        X.append(hada.tolist()[0])
+        line = [prot1,prot2]
+        X.append(line)
         y.append(0)
         sample_weight.append(mean)
 
@@ -86,11 +77,11 @@ for i in range(800, -1, -200):
         y_train, y_test = y[train_index], y[test_index]
         sample_weight_train = sample_weight[train_index]
         logging.info("Writing dataset with threshold: " + str(i) + " and fold: " + str(j))
-        np.savetxt('DB/Static/T'+str(i)+'/_X_train_Fold'+str(j)+'.csv', X_train, delimiter=',')
-        np.savetxt('DB/Static/T'+str(i)+'/_X_test_Fold'+str(j)+'.csv', X_test, delimiter=',')
-        np.savetxt('DB/Static/T'+str(i)+'/_y_train_Fold'+str(j)+'.csv', y_train, delimiter=',')
-        np.savetxt('DB/Static/T'+str(i)+'/_y_test_Fold'+str(j)+'.csv', y_test, delimiter=',')
-        np.savetxt('DB/Static/T'+str(i)+'/_sample_weight_train_Fold'+str(j)+'.csv', sample_weight_train, delimiter=',')
+        np.savetxt('DB/Static/T'+str(i)+'/X_train_Fold'+str(j)+'.csv', X_train, delimiter=',',fmt='%s')
+        np.savetxt('DB/Static/T'+str(i)+'/X_test_Fold'+str(j)+'.csv', X_test, delimiter=',',fmt='%s')
+        np.savetxt('DB/Static/T'+str(i)+'/y_train_Fold'+str(j)+'.csv', y_train, delimiter=',')
+        np.savetxt('DB/Static/T'+str(i)+'/y_test_Fold'+str(j)+'.csv', y_test, delimiter=',')
+        np.savetxt('DB/Static/T'+str(i)+'/sample_weight_train_Fold'+str(j)+'.csv', sample_weight_train, delimiter=',')
 
 logging.info("Starting Growing dataset building...")
 
@@ -110,24 +101,20 @@ for i in range(800, -1, -200):
     # Creating the dataset
     pairs_prots = []
     for d in data.values:
-        pairs_prots.append(('https://string-db.org/network/' + d[0],'https://string-db.org/network/' + d[1], 1))
+        pairs_prots.append((d[0].split('9606.ENSP')[1],d[1].split('9606.ENSP')[1], d[-1]))
 
     negative_pairs_prots = random.sample(all_negative_pairs_prots, len(pairs_prots))
 
     X, y, sample_weight = [], [], []
     for prot1, prot2, c in pairs_prots:
-        emb_prot1 = dict_embeddings[prot1].reshape(1, vector_size)
-        emb_prot2 = dict_embeddings[prot2].reshape(1, vector_size)
-        hada = np.multiply(emb_prot1, emb_prot2)
-        X.append(hada.tolist()[0])
+        line = [prot1,prot2]
+        X.append(line)
         y.append(1)
         sample_weight.append(c)
 
     for prot1, prot2, label in negative_pairs_prots:
-        emb_prot1 = dict_embeddings[prot1].reshape(1, vector_size)
-        emb_prot2 = dict_embeddings[prot2].reshape(1, vector_size)
-        hada = np.multiply(emb_prot1, emb_prot2)
-        X.append(hada.tolist()[0])
+        line = [prot1,prot2]
+        X.append(line)
         y.append(0)
         sample_weight.append(mean)
 
@@ -140,8 +127,8 @@ for i in range(800, -1, -200):
         y_train, y_test = y[train_index], y[test_index]
         sample_weight_train = sample_weight[train_index]
         logging.info("Writing dataset with threshold: " + str(i) + " and fold: " + str(j))
-        np.savetxt('DB/Growing/T'+str(i)+'/X_train_Fold'+str(j)+'.csv', X_train, delimiter=',')
-        np.savetxt('DB/Growing/T'+str(i)+'/X_test_Fold'+str(j)+'.csv', X_test, delimiter=',')
+        np.savetxt('DB/Growing/T'+str(i)+'/X_train_Fold'+str(j)+'.csv', X_train, delimiter=',',fmt='%s')
+        np.savetxt('DB/Growing/T'+str(i)+'/X_test_Fold'+str(j)+'.csv', X_test, delimiter=',',fmt='%s')
         np.savetxt('DB/Growing/T'+str(i)+'/y_train_Fold'+str(j)+'.csv', y_train, delimiter=',')
         np.savetxt('DB/Growing/T'+str(i)+'/y_test_Fold'+str(j)+'.csv', y_test, delimiter=',')
         np.savetxt('DB/Growing/T'+str(i)+'/sample_weight_train_Fold'+str(j)+'.csv', sample_weight_train, delimiter=',')
@@ -154,38 +141,34 @@ for i in range(800, -1, -200):
         data = data8
     elif i == 600:
         data6 = data_full.where(data_full['combined_score']>600).where(data_full['combined_score']<=800).dropna().reset_index(drop=True).sample(n=setsize,random_state=42)
-        data = data.append(data6)
+        data = pd.concat([data8,data6])
     elif i == 400:
         data4 = data_full.where(data_full['combined_score']>400).where(data_full['combined_score']<=600).dropna().reset_index(drop=True).sample(n=setsize,random_state=42)
-        data = data.append(data4)
+        data = pd.concat([data8,data6,data4])
     elif i == 200:
         data2 = data_full.where(data_full['combined_score']>200).where(data_full['combined_score']<=400).dropna().reset_index(drop=True).sample(n=setsize,random_state=42)
-        data = data.append(data2)
+        data = pd.concat([data8,data6,data4,data2])
     elif i == 0:
-        data0 = data_full.where(data_full['combined_score']<=200).sample(n=setsize,random_state=42)
-        data = data.append(data0)
+        data0 = data_full.where(data_full['combined_score']<=200).dropna().reset_index(drop=True).sample(n=setsize,random_state=42)
+        data = pd.concat([data8,data6,data4,data2,data0])
 
     # Creating the dataset
     pairs_prots = []
     for d in data.values:
-        pairs_prots.append(('https://string-db.org/network/' + d[0],'https://string-db.org/network/' + d[1], 1))
+        pairs_prots.append((d[0].split('9606.ENSP')[1],d[1].split('9606.ENSP')[1], d[-1]))
 
     negative_pairs_prots = random.sample(all_negative_pairs_prots, len(pairs_prots))
 
     X, y, sample_weight = [], [], []
     for prot1, prot2, c in pairs_prots:
-        emb_prot1 = dict_embeddings[prot1].reshape(1, vector_size)
-        emb_prot2 = dict_embeddings[prot2].reshape(1, vector_size)
-        hada = np.multiply(emb_prot1, emb_prot2)
-        X.append(hada.tolist()[0])
+        line = [prot1,prot2]
+        X.append(line)
         y.append(1)
         sample_weight.append(c)
 
     for prot1, prot2, label in negative_pairs_prots:
-        emb_prot1 = dict_embeddings[prot1].reshape(1, vector_size)
-        emb_prot2 = dict_embeddings[prot2].reshape(1, vector_size)
-        hada = np.multiply(emb_prot1, emb_prot2)
-        X.append(hada.tolist()[0])
+        line = [prot1,prot2]
+        X.append(line)
         y.append(0)
         sample_weight.append(mean)
 
@@ -198,8 +181,8 @@ for i in range(800, -1, -200):
         y_train, y_test = y[train_index], y[test_index]
         sample_weight_train = sample_weight[train_index]
         logging.info("Writing dataset with threshold: " + str(i) + " and fold: " + str(j))
-        np.savetxt('DB/Uniform/T'+str(i)+'/X_train_Fold'+str(j)+'.csv', X_train, delimiter=',')
-        np.savetxt('DB/Uniform/T'+str(i)+'/X_test_Fold'+str(j)+'.csv', X_test, delimiter=',')
+        np.savetxt('DB/Uniform/T'+str(i)+'/X_train_Fold'+str(j)+'.csv', X_train, delimiter=',',fmt='%s')
+        np.savetxt('DB/Uniform/T'+str(i)+'/X_test_Fold'+str(j)+'.csv', X_test, delimiter=',',fmt='%s')
         np.savetxt('DB/Uniform/T'+str(i)+'/y_train_Fold'+str(j)+'.csv', y_train, delimiter=',')
         np.savetxt('DB/Uniform/T'+str(i)+'/y_test_Fold'+str(j)+'.csv', y_test, delimiter=',')
         np.savetxt('DB/Uniform/T'+str(i)+'/sample_weight_train_Fold'+str(j)+'.csv', sample_weight_train, delimiter=',')
--- predictionModels.py
+++ predictionModels.py
@@ -3,23 +3,67 @@ from xgboost import XGBClassifier
 import numpy as np
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 import pandas as pd
+from pulearn import ElkanotoPuClassifier
+import logging
+from pathlib import Path
 
-classifiers = [
-    RandomForestClassifier(n_jobs=-1, random_state=42),
-    XGBClassifier(n_jobs=-1, random_state=42),
-]
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',level = logging.INFO,datefmt='%Y-%m-%d %H:%M:%S')
+
+def get_classifiers():
+    classifiers = [
+        RandomForestClassifier(n_jobs=-1, random_state=42),
+        XGBClassifier(n_jobs=-1, random_state=42),
+    ]
+    return classifiers
+
+embedCSV = pd.read_csv('DB/embeddings.csv',index_col=0)
+embeddings_array = np.array(list(embedCSV.values))
+dict_embeddings = {embedCSV.index[i]: embeddings_array[i] for i in range(len(embeddings_array))}
+vector_size = embedCSV.shape[1]
 
 for t in ['Uniform','Static','Growing']:
+    logging.info("Starting training with " + t + " dataset")
     for i in range(800, -1, -200):
-        for m in ['Normal','Weighted','PU']:
-            for c in classifiers:
-                metrics_to_csv = pd.DataFrame(columns=['precision', 'recall', 'WAF','accuracy','AUC'])
-                for f in range(10):
-                    X_train = np.loadtxt('DB/'+t+'/T'+str(i)+'/X_train_Fold'+str(f)+'.csv', delimiter=',')
-                    y_train = np.loadtxt('DB/'+t+'/T'+str(i)+'/y_train_Fold'+str(f)+'.csv', delimiter=',')
-                    X_test = np.loadtxt('DB/'+t+'/T'+str(i)+'/X_test_Fold'+str(f)+'.csv', delimiter=',')
-                    y_test = np.loadtxt('DB/'+t+'/T'+str(i)+'/y_test_Fold'+str(f)+'.csv', delimiter=',')
-                    sample_weight = np.loadtxt('DB/'+t+'/T'+str(i)+'/sample_weight_Fold'+str(f)+'.csv', delimiter=',')
+        logging.info("Starting training with threshold: " + str(i))
+        metrics_to_csv = {}
+        pd.DataFrame(columns=['precision', 'recall', 'WAF','accuracy','AUC'])
+        for f in range(10):
+            logging.info("Starting training with fold: " + str(f))
+            X_train1 = np.loadtxt('DB/'+t+'/T'+str(i)+'/X_train_Fold'+str(f)+'.csv', delimiter=',',dtype=str)
+            y_train = np.loadtxt('DB/'+t+'/T'+str(i)+'/y_train_Fold'+str(f)+'.csv', delimiter=',')
+            X_test1 = np.loadtxt('DB/'+t+'/T'+str(i)+'/X_test_Fold'+str(f)+'.csv', delimiter=',',dtype=str)
+            y_test = np.loadtxt('DB/'+t+'/T'+str(i)+'/y_test_Fold'+str(f)+'.csv', delimiter=',')
+            sample_weight = np.loadtxt('DB/'+t+'/T'+str(i)+'/sample_weight_train_Fold'+str(f)+'.csv', delimiter=',')
+
+            X_train = []
+            for prot1, prot2 in X_train1:
+                prot1 = 'https://string-db.org/network/9606.ENSP' + prot1
+                prot2 = 'https://string-db.org/network/9606.ENSP' + prot2
+                emb_prot1 = dict_embeddings[prot1].reshape(1, vector_size)
+                emb_prot2 = dict_embeddings[prot2].reshape(1, vector_size)
+                hada = np.multiply(emb_prot1, emb_prot2)
+                X_train.append(hada.tolist()[0])
+            
+            X_test = []
+            for prot1, prot2 in X_test1:
+                prot1 = 'https://string-db.org/network/9606.ENSP' + prot1
+                prot2 = 'https://string-db.org/network/9606.ENSP' + prot2
+                emb_prot1 = dict_embeddings[prot1].reshape(1, vector_size)
+                emb_prot2 = dict_embeddings[prot2].reshape(1, vector_size)
+                hada = np.multiply(emb_prot1, emb_prot2)
+                X_test.append(hada.tolist()[0])
+
+            X_train = np.array(X_train)
+            X_test = np.array(X_test)
+            for m in ['PU','Normal','Weighted']:
+                if m not in metrics_to_csv:
+                    metrics_to_csv[m] = {}
+                logging.info("Starting training with model: " + m)
+                clfs = get_classifiers()
+                for c in clfs:
+                    n = type(c).__name__
+                    if n not in metrics_to_csv[m]:
+                        metrics_to_csv[m][n] = pd.DataFrame(columns=['precision', 'recall', 'WAF','accuracy','AUC'])
                     if m == 'Weighted':
                         c.fit(X_train, y_train, sample_weight = sample_weight)
                         y_pred = c.predict(X_test)
@@ -36,8 +80,8 @@ for t in ['Uniform','Static','Growing']:
                     f1 = f1_score(y_test, y_pred, average='weighted')
                     acc = accuracy_score(y_test, y_pred)
                     auc = roc_auc_score(y_test, y_pred)
-                    metrics_to_csv = metrics_to_csv.append({'precision': prec, 'recall': rec, 'WAF': f1, 'accuracy': acc, 'AUC': auc}, ignore_index=True)
-                if m == 'PU':
-                    metrics_to_csv.to_csv('Results/7_03Test/'+t+'T'+str(i)+'metrics_'+m+c+'.csv', index=False)
-                else:
-                    metrics_to_csv.to_csv('Results/7_03Test/'+t+'T'+str(i)+'metrics_'+m+c+'.csv', index=False)
+                    metrics_to_csv[m][n].loc[len(metrics_to_csv[m][n].index)] = [prec, rec, f1, acc, auc]
+                    if f == 9:
+                        Path('Results/7_03Test/Folds/').mkdir(parents=True, exist_ok=True)
+                        metrics_to_csv[m][n].to_csv('Results/7_03Test/Folds/'+t+'T'+str(i)+'metrics_'+m+n+'.csv', index=False)
+                
