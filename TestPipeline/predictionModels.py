f467a8003c0f0b1225c6e6d95181b3dc27ff79bb
andre279m <mendes.andr843@gmail.com>
2024-05-23 12:22:28 +0100

    trying other weights

--- TestPipeline/predictionModels.py
+++ TestPipeline/predictionModels.py
@@ -23,64 +23,65 @@ vector_size = embedCSV.shape[1]
 
 for t in ['Undersampling', 'Uniform']: # Growing Static
     logging.info("Starting training with " + t + " dataset")
-    for i in range(800, -1, -200):
-        logging.info("Starting training with threshold: " + str(i))
-        metrics_to_csv = {}
-        for f in range(10):
-            logging.info("Starting training with fold: " + str(f))
-            X_train1 = np.loadtxt('../DB/'+t+'/T'+str(i)+'/X_train_Fold'+str(f)+'.csv', delimiter=',',dtype=str)
-            y_train = np.loadtxt('../DB/'+t+'/T'+str(i)+'/y_train_Fold'+str(f)+'.csv', delimiter=',')
-            X_test1 = np.loadtxt('../DB/'+t+'/T'+str(i)+'/X_test_Fold'+str(f)+'.csv', delimiter=',',dtype=str)
-            y_test = np.loadtxt('../DB/'+t+'/T'+str(i)+'/y_test_Fold'+str(f)+'.csv', delimiter=',')
-            sample_weight = np.loadtxt('../DB/'+t+'/T'+str(i)+'/sample_weight_train_Fold'+str(f)+'.csv', delimiter=',')
-            sample_weight = np.array([int(val/10) for val in sample_weight])
-            X_train = []
-            for prot1, prot2 in X_train1:
-                prot1 = 'https://string-db.org/network/9606.ENSP' + prot1
-                prot2 = 'https://string-db.org/network/9606.ENSP' + prot2
-                emb_prot1 = dict_embeddings[prot1].reshape(1, vector_size)
-                emb_prot2 = dict_embeddings[prot2].reshape(1, vector_size)
-                hada = np.multiply(emb_prot1, emb_prot2)
-                X_train.append(hada.tolist()[0])
-            
-            X_test = []
-            for prot1, prot2 in X_test1:
-                prot1 = 'https://string-db.org/network/9606.ENSP' + prot1
-                prot2 = 'https://string-db.org/network/9606.ENSP' + prot2
-                emb_prot1 = dict_embeddings[prot1].reshape(1, vector_size)
-                emb_prot2 = dict_embeddings[prot2].reshape(1, vector_size)
-                hada = np.multiply(emb_prot1, emb_prot2)
-                X_test.append(hada.tolist()[0])
+    #for i in range(800, -1, -200):
+    i = 0
+    logging.info("Starting training with threshold: " + str(i))
+    metrics_to_csv = {}
+    for f in range(10):
+        logging.info("Starting training with fold: " + str(f))
+        X_train1 = np.loadtxt('../DB/'+t+'/T'+str(i)+'/X_train_Fold'+str(f)+'.csv', delimiter=',',dtype=str)
+        y_train = np.loadtxt('../DB/'+t+'/T'+str(i)+'/y_train_Fold'+str(f)+'.csv', delimiter=',')
+        X_test1 = np.loadtxt('../DB/'+t+'/T'+str(i)+'/X_test_Fold'+str(f)+'.csv', delimiter=',',dtype=str)
+        y_test = np.loadtxt('../DB/'+t+'/T'+str(i)+'/y_test_Fold'+str(f)+'.csv', delimiter=',')
+        sample_weight = np.loadtxt('../DB/'+t+'/T'+str(i)+'/sample_weight_train_Fold'+str(f)+'.csv', delimiter=',')
+        sample_weight = np.array([int(val/100) for val in sample_weight])
+        X_train = []
+        for prot1, prot2 in X_train1:
+            prot1 = 'https://string-db.org/network/9606.ENSP' + prot1
+            prot2 = 'https://string-db.org/network/9606.ENSP' + prot2
+            emb_prot1 = dict_embeddings[prot1].reshape(1, vector_size)
+            emb_prot2 = dict_embeddings[prot2].reshape(1, vector_size)
+            hada = np.multiply(emb_prot1, emb_prot2)
+            X_train.append(hada.tolist()[0])
+        
+        X_test = []
+        for prot1, prot2 in X_test1:
+            prot1 = 'https://string-db.org/network/9606.ENSP' + prot1
+            prot2 = 'https://string-db.org/network/9606.ENSP' + prot2
+            emb_prot1 = dict_embeddings[prot1].reshape(1, vector_size)
+            emb_prot2 = dict_embeddings[prot2].reshape(1, vector_size)
+            hada = np.multiply(emb_prot1, emb_prot2)
+            X_test.append(hada.tolist()[0])
 
-            X_train = np.array(X_train)
-            X_test = np.array(X_test)
-            for m in ['Weighted']:# 'PU','Normal',
-                if m not in metrics_to_csv:
-                    metrics_to_csv[m] = {}
-                logging.info("Starting training with model: " + m)
-                clfs = get_classifiers()
-                for c in clfs:
-                    n = type(c).__name__
-                    if n not in metrics_to_csv[m]:
-                        metrics_to_csv[m][n] = pd.DataFrame(columns=['precision', 'recall', 'WAF','accuracy','AUC'])
-                    if m == 'Weighted':
-                        c.fit(X_train, y_train, sample_weight = sample_weight)
-                        y_pred = c.predict(X_test)
-                    elif m == 'PU':
-                        pu = ElkanotoPuClassifier(estimator = c, hold_out_ratio = 0.1)
-                        y_train_pu = np.where(y_train == 0, -1, y_train)
-                        pu.fit(X_train, y_train)
-                        y_pred = pu.predict(X_test)
-                    else:
-                        c.fit(X_train, y_train)
-                        y_pred = c.predict(X_test)
-                    prec = precision_score(y_test, y_pred, average='weighted')
-                    rec = recall_score(y_test, y_pred, average='weighted')
-                    f1 = f1_score(y_test, y_pred, average='weighted')
-                    acc = accuracy_score(y_test, y_pred)
-                    auc = roc_auc_score(y_test, y_pred)
-                    metrics_to_csv[m][n].loc[len(metrics_to_csv[m][n].index)] = [prec, rec, f1, acc, auc]
-                    if f == 9:
-                        Path('../Results/18_05Test/Folds/').mkdir(parents=True, exist_ok=True)
-                        metrics_to_csv[m][n].to_csv('../Results/18_05Test/Folds/'+t+'T'+str(i)+'metrics_'+m+n+'.csv', index=False)
-                
+        X_train = np.array(X_train)
+        X_test = np.array(X_test)
+        for m in ['Weighted']:# 'PU','Normal',
+            if m not in metrics_to_csv:
+                metrics_to_csv[m] = {}
+            logging.info("Starting training with model: " + m)
+            clfs = get_classifiers()
+            for c in clfs:
+                n = type(c).__name__
+                if n not in metrics_to_csv[m]:
+                    metrics_to_csv[m][n] = pd.DataFrame(columns=['precision', 'recall', 'WAF','accuracy','AUC'])
+                if m == 'Weighted':
+                    c.fit(X_train, y_train, sample_weight = sample_weight)
+                    y_pred = c.predict(X_test)
+                elif m == 'PU':
+                    pu = ElkanotoPuClassifier(estimator = c, hold_out_ratio = 0.1)
+                    y_train_pu = np.where(y_train == 0, -1, y_train)
+                    pu.fit(X_train, y_train)
+                    y_pred = pu.predict(X_test)
+                else:
+                    c.fit(X_train, y_train)
+                    y_pred = c.predict(X_test)
+                prec = precision_score(y_test, y_pred, average='weighted')
+                rec = recall_score(y_test, y_pred, average='weighted')
+                f1 = f1_score(y_test, y_pred, average='weighted')
+                acc = accuracy_score(y_test, y_pred)
+                auc = roc_auc_score(y_test, y_pred)
+                metrics_to_csv[m][n].loc[len(metrics_to_csv[m][n].index)] = [prec, rec, f1, acc, auc]
+                if f == 9:
+                    Path('../Results/18_05Test/Folds/').mkdir(parents=True, exist_ok=True)
+                    metrics_to_csv[m][n].to_csv('../Results/18_05Test/Folds/'+t+'T'+str(i)+'metrics_'+m+n+'.csv', index=False)
+            
