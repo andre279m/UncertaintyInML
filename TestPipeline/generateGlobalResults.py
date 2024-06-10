af341edc641e845188edd52a43177e4295944203
andre279m <mendes.andr843@gmail.com>
2024-06-10 11:31:02 +0100

    confirm results

--- TestPipeline/generateGlobalResults.py
+++ TestPipeline/generateGlobalResults.py
@@ -3,15 +3,15 @@ import logging
 
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',level = logging.INFO,datefmt='%Y-%m-%d %H:%M:%S')
 
-for t in ['Uniform','Undersampling']:
+for t in ['Growing', 'Static', 'Undersampling', 'Uniform']:
     logging.info("Starting training with " + t + " dataset")
-    for m in ['Weighted']:
-        for clf in ["RandomForestClassifier","XGBClassifier"]:
+    for m in ['Weighted', 'Normal']:
+        for clf in ["RandomForestClassifier","XGBClassifier","LinearSVC","LogisticRegression","GaussianNB","CatBoostClassifier","SGDClassifier"]:
             global_results = pd.DataFrame(columns=['PRECISION(MEDIAN)','PRECISION(IQR)','RECALL(MEDIAN)','RECALL(IQR)','WAF(MEDIAN)','WAF(IQR)','ACCURACY(MEDIAN)','ACCURACY(IQR)','ROC AUC(MEDIAN)','ROC AUC(IQR)'])
             for i in range(800, -1, -200):
-                d = pd.read_csv('../Results/18_05Test/Folds/' + t + 'T' + str(i) + 'metrics_' + m + clf + '.csv',header=0)
+                d = pd.read_csv('../Results/6_06Test/Folds/' + t + 'T' + str(i) + 'metrics_' + m + clf + '.csv',header=0)
                 global_results.loc[i] = {'PRECISION(MEDIAN)':d['precision'].median(),'PRECISION(IQR)':d['precision'].quantile(0.75)-d['precision'].quantile(0.25),'RECALL(MEDIAN)':d['recall'].median(),'RECALL(IQR)':d['recall'].quantile(0.75)-d['recall'].quantile(0.25),'WAF(MEDIAN)':d['WAF'].median(),'WAF(IQR)':d['WAF'].quantile(0.75)-d['WAF'].quantile(0.25),'ACCURACY(MEDIAN)':d['accuracy'].median(),'ACCURACY(IQR)':d['accuracy'].quantile(0.75)-d['accuracy'].quantile(0.25),'ROC AUC(MEDIAN)':d['AUC'].median(),'ROC AUC(IQR)':d['AUC'].quantile(0.75)-d['AUC'].quantile(0.25)}
-            global_results.to_csv('../Results/18_05Test/' + t + 'metrics_' + m + clf + '.csv')
+            global_results.to_csv('../Results/6_06Test/' + t + 'metrics_' + m + clf + '.csv')
 
 
 # for Random testing
