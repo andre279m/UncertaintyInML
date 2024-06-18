cf3a7fb9a1d5f91dbc53e77ae4dd81fd0652519d
andre279m <mendes.andr843@gmail.com>
2024-06-18 10:34:52 +0100

    stat results

--- TestPipeline/generateStatsResults.py
+++ TestPipeline/generateStatsResults.py
@@ -20,18 +20,18 @@ for i in range(800, -1, -200):
             if t1 != t2:
                 for m in ['Normal','Weighted','PU']:
                     for clf in ["RandomForestClassifier","XGBClassifier"]:
-                        d1 = pd.read_csv('Results/22_03Test/Folds/' + t1 + 'T' + str(i) + 'metrics_' + m + clf + '.csv',header=0)
-                        d2 = pd.read_csv('Results/22_03Test/Folds/' + t2 + 'T' + str(i) + 'metrics_' + m + clf + '.csv',header=0)
+                        d1 = pd.read_csv('../Results/12_06Test/Folds/' + t1 + 'T' + str(i) + 'metrics_' + m + clf + '.csv',header=0)
+                        d2 = pd.read_csv('../Results/12_06Test/Folds/' + t2 + 'T' + str(i) + 'metrics_' + m + clf + '.csv',header=0)
                         w = run_statiscal_test(d1['WAF'].values, d2['WAF'].values,test='Kruskal')
                         a = run_statiscal_test(d1['accuracy'].values, d2['accuracy'].values,test='Kruskal')
                         auc = run_statiscal_test(d1['AUC'].values, d2['AUC'].values,test='Kruskal')
                         accDF.loc[m + clf,t1 + 'vs' + t2] = a
                         wafDF.loc[m + clf,t1 + 'vs' + t2] = w
                         rocDF.loc[m + clf,t1 + 'vs' + t2] = auc
-    Path('Results/22_03Test/StatisticalTests/').mkdir(parents=True, exist_ok=True)
-    accDF.to_csv('Results/22_03Test/StatisticalTests/DBaccT' + str(i) + '.csv')
-    wafDF.to_csv('Results/22_03Test/StatisticalTests/DBwafT' + str(i) + '.csv')
-    rocDF.to_csv('Results/22_03Test/StatisticalTests/DBrocT' + str(i) + '.csv')
+    Path('../Results/12_06Test/StatisticalTests/').mkdir(parents=True, exist_ok=True)
+    accDF.to_csv('../Results/12_06Test/StatisticalTests/DBaccT' + str(i) + '.csv')
+    wafDF.to_csv('../Results/12_06Test/StatisticalTests/DBwafT' + str(i) + '.csv')
+    rocDF.to_csv('../Results/12_06Test/StatisticalTests/DBrocT' + str(i) + '.csv')
 
 for i in range(800, -1, -200):
     accDF = pd.DataFrame()
@@ -42,15 +42,15 @@ for i in range(800, -1, -200):
             if m1 != m2:
                 for t in ['Static','Growing','Uniform']:
                     for clf in ["RandomForestClassifier","XGBClassifier"]:
-                        d1 = pd.read_csv('Results/22_03Test/Folds/' + t + 'T' + str(i) + 'metrics_' + m1 + clf + '.csv',header=0)
-                        d2 = pd.read_csv('Results/22_03Test/Folds/' + t + 'T' + str(i) + 'metrics_' + m2 + clf + '.csv',header=0)
+                        d1 = pd.read_csv('../Results/12_06Test/Folds/' + t + 'T' + str(i) + 'metrics_' + m1 + clf + '.csv',header=0)
+                        d2 = pd.read_csv('../Results/12_06Test/Folds/' + t + 'T' + str(i) + 'metrics_' + m2 + clf + '.csv',header=0)
                         w = run_statiscal_test(d1['WAF'].values, d2['WAF'].values,test='Kruskal')
                         a = run_statiscal_test(d1['accuracy'].values, d2['accuracy'].values,test='Kruskal')
                         auc = run_statiscal_test(d1['AUC'].values, d2['AUC'].values,test='Kruskal')
                         accDF.loc[t + clf,m1 + 'vs' + m2] = a
                         wafDF.loc[t + clf,m1 + 'vs' + m2] = w
                         rocDF.loc[t + clf,m1 + 'vs' + m2] = auc
-    Path('Results/22_03Test/StatisticalTests/').mkdir(parents=True, exist_ok=True)
-    accDF.to_csv('Results/22_03Test/StatisticalTests/MLaccT' + str(i) + '.csv')
-    wafDF.to_csv('Results/22_03Test/StatisticalTests/MLwafT' + str(i) + '.csv')
-    rocDF.to_csv('Results/22_03Test/StatisticalTests/MLrocT' + str(i) + '.csv')
+    Path('../Results/12_06Test/StatisticalTests/').mkdir(parents=True, exist_ok=True)
+    accDF.to_csv('../Results/12_06Test/StatisticalTests/MLaccT' + str(i) + '.csv')
+    wafDF.to_csv('../Results/12_06Test/StatisticalTests/MLwafT' + str(i) + '.csv')
+    rocDF.to_csv('../Results/12_06Test/StatisticalTests/MLrocT' + str(i) + '.csv')
